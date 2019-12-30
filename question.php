97370fdcd481ff30998cd5b43e8acdabe0184f84
DEVELOPERfalcon <dhdyddus3@naver.com>
2019-12-30 13:51:51 +0900

    question & style modify

---
+++ question.php
@@ -0,0 +1,29 @@
+<?php
+	header('Content-Type:text/html; charset=utf-8');
+	$connect=mysqli_connect("localhost", "developer3", "qwas2468!", "developer3");
+	mysqli_query($connect, "set names utf8");
+	$sql="select No, title, writeDate, count from question order by No desc";
+	$result=mysqli_query($connect, $sql);
+	$rowCount=mysqli_num_rows($result);
+    
+	$table="<table border=\"1\" style='width:100%; text-align: center;'><colgroup><col width=\"80\"px><col><col width=\"100\"px><col width=\"80\"px></colgroup><tr><td>No</td><td>제목</td><td>작성시간</td><td>조회</td></tr>";
+	
+	for($i=0;$i<$rowCount;$i++){
+	    $row=mysqli_fetch_array($result, MYSQLI_ASSOC);
+	    $table.="<tr>";
+	    foreach ($row as $key => $value) {
+	    	if($key=="title"){
+                $table.="<td style='text-align:left;'><a href='http://developer3.dothome.co.kr/WebProject/question2.php?No=".($rowCount-$i)."'>".$value."</a></td>";
+            }else{
+                $table.="<td>".$value."</td>";
+            }
+	    }
+	    $table.="</tr>";
+	}
+	$table.="</table>";
+    echo $table;
+    echo "<br>";
+	
+	mysqli_close($connect);
+
+ ?>
--- writeQuestion.php
+++ writeQuestion.php
@@ -1,16 +1,14 @@
 <?php
 	header('Content-Type:text/html; charset=utf-8');
     $title=$_POST['title'];
-    $type=$_POST['type'];
-    $date=$_POST['date'];
     $content=$_POST['content'];
     $now = date("Y-m-d h:i:s");
 
 	$connect=mysqli_connect("localhost", "developer3", "qwas2468!", "developer3");
 	mysqli_query($connect, "set names utf8");
-	$sql="insert into bug(title, type, content, occurDate, writeDate) value('$title', '$type', '$content', '$date', '$now')";
+	$sql="insert into question(title, content, writeDate) value('$title', '$content', '$now')";
 	$result=mysqli_query($connect, $sql);
-	if($result) include './bug.html';
+	if($result) include './question.html';
 	else echo "저장 실패하였습니다.";
 	mysqli_close($connect);
 ?>
